bfc05e4d96f15fa4237b888f2aff370400206ef3
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2025-09-09 19:34:58 -0500

    Update UserMenu.php

--- Widgets/UserMenu.php
+++ Widgets/UserMenu.php
@@ -28,26 +28,25 @@ class UserMenu {
         $titleHtml = htmlspecialchars($title);
         $profileLinkHtml = htmlspecialchars($profile_link);
         $signoutLinkHtml = htmlspecialchars($signout_link);
-
-        return <<<'HTML'
-            <li class="nav-item dropdown user-menu">
-              <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                <img src="{$imageHtml}" class="user-image rounded-circle shadow" alt="User Image">
-                <span class="d-none d-md-inline">{$nameHtml}</span>
+        
+        return "
+            <li class=\"nav-item dropdown user-menu\">
+              <a href=\"#\" class=\"nav-link dropdown-toggle\" data-bs-toggle=\"dropdown\">
+                <img src=\"{$imageHtml}\" class=\"user-image rounded-circle shadow\" alt=\"User Image\">
+                <span class=\"d-none d-md-inline\">{$usernameHtml}</span>
               </a>
-              <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
-                <li class="user-header text-bg-primary">
-                  <img src="{$imageHtml}" class="rounded-circle shadow" alt="User Image">
+              <ul class=\"dropdown-menu dropdown-menu-lg dropdown-menu-end\">
+                <li class=\"user-header text-bg-secondary\">
+                  <img src=\"{$imageHtml}\" class=\"rounded-circle shadow\" alt=\"User Image\">
                   <p>
-                    {$usernameHtml} - {$titleHtml}
+                    {$nameHtml} <br/> {$titleHtml}
                   </p>
                 </li>
-                <li class="user-footer">
-                  <a href="{$profileLinkHtml}" class="btn btn-default btn-flat">Profile</a>
-                  <a href="{$signoutLinkHtml}" class="btn btn-default btn-flat float-end">Sign out</a>
+                <li class=\"user-footer\">
+                  <a href=\"{$profileLinkHtml}\" class=\"btn btn-default btn-flat\">Perfil</a>
+                  <a href=\"{$signoutLinkHtml}\" class=\"btn btn-default btn-flat float-end\">Cerrar Sesión</a>
                 </li>
               </ul>
-            </li>
-HTML;
+            </li>";
     }
 }
